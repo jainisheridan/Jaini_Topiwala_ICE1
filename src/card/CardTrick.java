7809d7ac3e9a232e2b31d80cdd19eca9f205a7a1
jainisheridan <topiwalj@sheridancollege.ca>
2023-05-26 23:14:37 -0400

    branch main with name and Id

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -1,6 +1,8 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * @author Jaini Bhaveshbhai Topiwala
+ * Student Id :- 991669857
  */
 package card;
 
